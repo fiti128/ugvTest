208f22b2f8a236af24e9c058690820df01ddee00
unknown <tr-legres@yandex.ru>
2013-05-12 11:28:37 +0400

    some changes

--- src/main/java/rw/ugv/Main.java
+++ src/main/java/rw/ugv/Main.java
@@ -1,8 +1,13 @@
 package rw.ugv;
 
 import java.sql.Timestamp;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
+
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,6 +16,7 @@ import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
 import rw.ugv.dto.UgvDocument;
+import rw.ugv.dto.UgvOperation;
 
 public class Main {
 
@@ -19,22 +25,39 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		UgvDocument document = new UgvDocument();
-//		document.setCreatingTime(new Timestamp(new Date().getTime()));
-//		document.setCreatingTime(new Date());
 		document.setDocumentDate("10-05");
 		document.setDocumentNumber("5");
-//		System.out.println(document.getTest().getStr());
-		document.setForm("VP-58");
+		String form = "VP-85";
+		document.setForm("VP-85");
+		
+		UgvOperation uo = new UgvOperation();
+		Calendar cal = Calendar.getInstance();
+		cal.set(2012, Calendar.APRIL, 15);
+		uo.setOpenDateInAB(cal);
+		Calendar closeCal = Calendar.getInstance();
+		closeCal.set(2013, Calendar.APRIL, 18);
+		uo.setClosingDateInAB(closeCal);
+		uo.setPriznakZapisi('Z');
+//		uo.setUgvDocId(document);
+		
 		Configuration conf = new Configuration().configure();
+		conf.addAnnotatedClass(UgvOperation.class);
 		ServiceRegistry sr = new ServiceRegistryBuilder().applySettings(conf.getProperties()).buildServiceRegistry();
 		SessionFactory sf = conf.buildSessionFactory(sr);
 	
 		Session session = sf.openSession();
 		Transaction trans = session.beginTransaction();
-		session.save(document);
+		Query query = session.createQuery("from UgvDocument as doc where doc.form = 'VP-85'");
+		List<UgvDocument> list = query.list();
+		System.out.println(list.get(0).getForm());
+		UgvDocument document1 = list.get(0);
+		uo.setUgvDocId(document1);
+		session.save(uo);
 		trans.commit();
 		
 		session.close();
+		
+
 
 	}
 
--- src/main/java/rw/ugv/dto/UgvOperation.java
+++ src/main/java/rw/ugv/dto/UgvOperation.java
@@ -1,17 +1,25 @@
 package rw.ugv.dto;
 
+import java.io.Serializable;
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "VGO")
-public class UgvOperation {
+public class UgvOperation implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7251917582711374038L;
 
 	public UgvOperation() {
 		creatingTime = new Date();
@@ -20,5 +28,60 @@ public class UgvOperation {
 	@Temporal( value = TemporalType.TIMESTAMP ) 
 	@Column (name = "ZAP_ID", nullable = false, updatable = false)
 	private Date creatingTime;
+	
+	@Temporal( value = TemporalType.TIMESTAMP ) 
+	@Column (name = "DATOP")
+	private Calendar openDateInAB;
+	
+	@Temporal( value = TemporalType.TIMESTAMP ) 
+	@Column (name = "DATOP_END")
+	private Calendar closingDateInAB;
+	
+	@Column (name = "PR_ZAP")
+	private char priznakZapisi;
+	
+	@ManyToOne
+	private UgvDocument ugvDocId;
+
+	public Date getCreatingTime() {
+		return creatingTime;
+	}
+
+	public Calendar getOpenDateInAB() {
+		return openDateInAB;
+	}
+
+	public Calendar getClosingDateInAB() {
+		return closingDateInAB;
+	}
+
+	public char getPriznakZapisi() {
+		return priznakZapisi;
+	}
+
+	public UgvDocument getUgvDocId() {
+		return ugvDocId;
+	}
+
+	public void setCreatingTime(Date creatingTime) {
+		this.creatingTime = creatingTime;
+	}
+
+	public void setOpenDateInAB(Calendar openDateInAB) {
+		this.openDateInAB = openDateInAB;
+	}
+
+	public void setClosingDateInAB(Calendar closingDateInAB) {
+		this.closingDateInAB = closingDateInAB;
+	}
+
+	public void setPriznakZapisi(char priznakZapisi) {
+		this.priznakZapisi = priznakZapisi;
+	}
+
+	public void setUgvDocId(UgvDocument ugvDocId) {
+		this.ugvDocId = ugvDocId;
+	}
+	
 	
 }
